4fe6940e0f4918a18158d03c0b721a3f70c12aaa
Ben McClung <aurumvalian@gmail.com>
2020-10-22 20:47:30 -0600

    Add FrequencyPage styling

--- src/FrequencyPage/FrequencyPage.js
+++ src/FrequencyPage/FrequencyPage.js
@@ -21,18 +21,76 @@ export default FrequencyPage = () => {
     return <AppLoading/>
   } else {
     return (
-      <View>
+      <View style={styles.container}>
         <Header />
-        <Text>When should I send your MediMinders?</Text>
-        <Text>Frequency</Text>
-        <View>
-        <Switch><Text>Monday through Friday</Text></Switch>
-        <Switch><Text>Every Day</Text></Switch>
-        <Switch><Text>Custom</Text></Switch>
+        <Text style={styles.welcomeText}>When should I send your reminder?</Text>
+        <View style={styles.frequencyBox}>
+          <Text style={styles.welcomeText}>Frequency</Text>
+          <Text style={styles.dateLabel}>Monday through Friday</Text>
+          <Switch></Switch>
+          <Text style={styles.dateLabel}>Every Day</Text>
+          <Switch></Switch>
+          <Text style={styles.dateLabel}>Custom</Text>
+          <Switch></Switch>
         </View>
-        <Text>Time</Text>
-        <TextInput placeholder="datepicker"></TextInput>
+        <View style={styles.frequencyBox}>
+          <Text style={styles.welcomeText}>Time</Text>
+          <TextInput placeholder="datepicker" style={styles.inputText}></TextInput>
+        </View>
+        <TouchableOpacity style={styles.buttonStyle}>
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: white,
+    // alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  buttonContainer: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  welcomeText: {
+    color: lightBlue,
+    fontSize: 26,
+    fontFamily: "Montserrat_700Bold",
+  },
+  inputText: {
+    color: red,
+    fontSize: 38,
+    fontFamily: "Montserrat_700Bold",
+    marginTop: 40,
+    textAlign: "center",
+    // borderBottom: red
+  },
+  buttonText: {
+    color: white,
+    fontFamily: "Montserrat_700Bold",
+    fontSize: 20,
+    textAlign: "center"
+  },
+  buttonStyle: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    backgroundColor: red,
+    padding: 13,
+    borderRadius: 10,
+    marginTop: 20,
+    width: "60%",
+  },
+  frequencyBox: {
+    marginTop: 20
+  },
+  dateLabel: {
+    fontSize: 16,
+    marginTop: 10
+  }
+
+})
