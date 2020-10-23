4665486d2540689cbf9d1a71de8c791d0b5dbae8
Nick Hart <nick.hart.des@gmail.com>
2020-10-23 14:00:06 -0600

    Set up NavigationContainer

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, {Component} from 'react';
 import {store} from './src/store'
 
-// import {NavigationContainer} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -19,16 +19,18 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Stack.Navigator 
-      style={styles.container}
-      initialRouteName="ReminderSettingPage"
-    >
-      <Stack.Screen name="LandingPage" component={LandingPage} />
-      <Stack.Screen name="ReminderSettingPage" component={ReminderSettingPage} />
-      <Stack.Screen name="FrequencyPage" component={FrequencyPage} />
-      <Stack.Screen name="ProfilePage" component={ProfilePage} />
-      <StatusBar style="auto" />
-    </Stack.Navigator>
+      <NavigationContainer>
+        <Stack.Navigator 
+          style={styles.container}
+          initialRouteName="LandingPage"
+        >
+          <Stack.Screen name="LandingPage" component={LandingPage} />
+          <Stack.Screen name="ReminderSettingPage" component={ReminderSettingPage} />
+          <Stack.Screen name="FrequencyPage" component={FrequencyPage} />
+          <Stack.Screen name="ProfilePage" component={ProfilePage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      // <StatusBar style="auto" />
   );
 }
 
