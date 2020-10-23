6195bcbcc0d8329405b64b521ca7f4b22f7f5c88
Nick Hart <nick.hart.des@gmail.com>
2020-10-23 13:31:05 -0600

    Begin createStackNavigator setup

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 import {store} from './src/store'
 
 // import {NavigationContainer} from '@react-navigation/native'
-// import {createStackNavigator} from '@react-navigation/stack'
+import { createStackNavigator } from 'react-navigation-stack'
 import { StyleSheet, Text, View } from 'react-native';
 
 
@@ -14,14 +14,14 @@ import ReminderSettingPage from './src/ReminderSettingPage/ReminderSettingPage'
 import FrequencyPage from './src/FrequencyPage/FrequencyPage'
 import ProfilePage from './src/Profile/ProfilePage'
 
-
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Stack.Navigator style={styles.container}>
       <LandingPage />
       <StatusBar style="auto" />
-    </View>
+    </Stack.Navigator>
   );
 }
 
