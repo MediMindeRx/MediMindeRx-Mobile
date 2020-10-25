e0f0b4cf460115b8ffed0249db8c1b7461c26a81
Nick Hart <nick.hart.des@gmail.com>
2020-10-24 22:01:41 -0600

    Further configure setNotificationHandler

--- App.js
+++ App.js
@@ -17,8 +17,8 @@ console.disableYellowBox = true;
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
+    shouldPlaySound: true,
+    shouldSetBadge: true
   }),
 });
 
