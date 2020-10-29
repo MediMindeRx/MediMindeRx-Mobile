5dfcf617329b776eade7da1a90f7f5ad5d750c12
Ben McClung <aurumvalian@gmail.com>
2020-10-29 11:38:31 -0600

    Add input field testing on landing page

--- src/LandingPage/LandingPage.test.js
+++ src/LandingPage/LandingPage.test.js
@@ -10,8 +10,20 @@ describe('LandingPage', async () => {
 
     const topGreeting = await waitFor(() => getByText('Hey there!'))
     const bottomGreeting = await waitFor(() => getByText('What\'s your name?'))
-    
+
     expect(topGreeting).toHaveTextContent('Hey there!')
     expect(bottomGreeting).toHaveTextContent('What\'s your name?')
   });
+
+  it('should have an input field', async () => {
+    const { findByPlaceholderText } = render(<LandingPage />)
+
+    const input = await waitFor(() => findByPlaceholderText('Name'))
+
+    expect(input).toHaveTextContent("");
+  })
+
+  it('input field should be changeable', async () => {
+    
+  })
 });
